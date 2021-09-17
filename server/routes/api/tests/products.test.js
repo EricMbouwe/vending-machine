325a914b5b281ac90ba941a21241f62e3a209e42
ericmbouwe <ericmbouwe@yahoo.fr>
2021-09-17 06:20:02 +0100

    add auth tests for products by user role

--- server/routes/api/tests/products.test.js
+++ server/routes/api/tests/products.test.js
@@ -10,7 +10,14 @@ beforeAll((done) => {
     .post('/auth/login')
     .send({ username: 'thomas', password: '123456' })
     .end((err, response) => {
-      console.log('RESPONSE', response.body);
+      cookie = response.headers['set-cookie'];
+      done();
+    });
+
+  buyer
+    .post('/auth/login')
+    .send({ username: 'santiago', password: '123456' })
+    .end((err, response) => {
       cookie = response.headers['set-cookie'];
       done();
     });
@@ -18,14 +25,30 @@ beforeAll((done) => {
 
 describe('API/PRODUCTS', () => {
   // Token not being sent - should respond with a 401
-  test('It should require authorization', () => {
+  test('It should require authentication from anonymous request', () => {
+    return request(app)
+      .get('/api/products')
+      .then((response) => {
+        expect(response.statusCode).toBe(401);
+      });
+  });
+
+  // Token not being sent - should respond with a 401
+  test('It should require authentication from a seller user', () => {
+    return seller.get('/api/products').then((response) => {
+      expect(response.statusCode).toBe(401);
+    });
+  });
+
+  // Token not being sent - should respond with a 401
+  test('It should require authentication from a buyer user', () => {
     return seller.get('/api/products').then((response) => {
       expect(response.statusCode).toBe(401);
     });
   });
 
   // Send the token - should respond with a 200
-  test('It responds with JSON', () => {
+  test('It responds with JSON from a seller user request', () => {
     return seller
       .get('/api/products')
       .set('Cookie', cookie)
@@ -35,8 +58,37 @@ describe('API/PRODUCTS', () => {
       });
   });
 
-  it('GET /api/products -> array of products', () => {
+  test('It responds with JSON from a seller buyer request', () => {
+    return buyer
+      .get('/api/products')
+      .set('Cookie', cookie)
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toBe('application/json');
+      });
+  });
+
+  test('Seller GET /api/products -> array of products', () => {
     return seller
+      .get('/api/products')
+      .set('Cookie', cookie)
+      .expect('Content-type', /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              productName: expect.any(String),
+              cost: expect.any(Number),
+              sellerId: expect.any(Number),
+            }),
+          ]),
+        );
+      });
+  });
+
+  test('Buyer GET /api/products -> array of products', () => {
+    return buyer
       .get('/api/products')
       .set('Cookie', cookie)
       .expect('Content-type', /json/)
